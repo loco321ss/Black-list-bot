d9e5a10b44b2e70e9d05f3fde3b653ee21a8379b
loco321ss <51173841+loco321ss@users.noreply.github.com>
2026-07-17 17:36:50 -0300

    Bot.py

---
+++ Bot.py
@@ -0,0 +1,50 @@
+import discord
+from discord.ext import commands
+import os
+
+TOKEN = os.getenv("TOKEN")
+
+CANAL_VOZ = "The Black list"
+
+intents = discord.Intents.default()
+intents.voice_states = True
+intents.guilds = True
+intents.members = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+conectado = False
+
+
+@bot.event
+async def on_ready():
+    print(f"Bot conectado como {bot.user}")
+
+
+@bot.event
+async def on_voice_state_update(member, before, after):
+    global conectado
+
+    if member.bot:
+        return
+
+    canal = discord.utils.get(member.guild.voice_channels, name=CANAL_VOZ)
+
+    if canal is None:
+        return
+
+    personas = [m for m in canal.members if not m.bot]
+
+    if len(personas) >= 2 and not conectado:
+        await canal.connect()
+        conectado = True
+        print("Entré al canal")
+
+    elif len(personas) < 2 and conectado:
+        if canal.guild.voice_client:
+            await canal.guild.voice_client.disconnect()
+        conectado = False
+        print("Salí del canal")
+
+
+bot.run(TOKEN)
